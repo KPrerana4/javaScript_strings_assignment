2b68ff322c4f580b48b38d2c559e11d027e1c0fa
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-03-02 10:49:46 +0530

    using reduce method

--- camelCase.js
+++ camelCase.js
@@ -4,14 +4,23 @@ function camelCase(words)
     for(let index = 1; index < words.length; index++)
     {
         let word = words[index];
-        sentence += word[0].toUpperCase() + word.substring(1,word.length);
+        sentence += word[0].toUpperCase() + word.slice(1).toLowerCase();
     }
     return sentence;
 }
 
+function getCamelCasedNotation(words)
+{
+    words[0] = words[0].toLowerCase();
+    return words.reduce(function(camelCasedNotation, word){
+        return camelCasedNotation + word[0].toUpperCase()+word.slice(1).toLowerCase();
+    });
+}
+
 function main()
 {
-    let words = ["this", "is", "sentence"];
+    let words = ["This", "iS", "a", "seNTence"];
+    console.log("Camel Case : " + getCamelCasedNotation(words));
     console.log("Camel Case : " + camelCase(words));
 }
 
